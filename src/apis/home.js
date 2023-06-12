cae60dffb96b2641e3f22d98a21bfd5e9bc18c57
Szevasz <Szevasz@163.com>
2023-06-12 21:53:29 +0800

    新鲜好物实现

--- src/apis/home.js
+++ src/apis/home.js
@@ -2,6 +2,17 @@ import httpInstance from "@/utils/http";
 
 export function getBannerAPI() {
     return httpInstance({
-        url:'/home/banner'
+        url: '/home/banner'
+    })
+}
+
+// /**
+//  * @description: 获取新鲜好物
+//  * @param {*}
+//  * @return {*}
+//  */
+export function findNewAPI() {
+    return httpInstance({
+        url: '/home/new'
     })
 }
